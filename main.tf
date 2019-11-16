b3144e7132b543953bf2d77f77d2fb6162d1ee2f
nuclearnic <nicholas@nuclearnic.net>
2019-11-16 22:51:36 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  access_key                  = "mockkeyid"
+  region                      = "us-east-1"
+  s3_force_path_style         = true
+  secret_key                  = "mockkeysecret"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    dynamodb       = "http://localhost:4569"
+    kinesis        = "http://localhost:4568"
+    lambda         = "http://localhost:4574"
+    s3             = "http://localhost:4572"
+    sns            = "http://localhost:4575"
+    sqs            = "http://localhost:4576"
+  }
+}
+
+# TODO
+# module "dynamodb" {
+#     source = "git::ssh://<YOUR_REPO>//modules/dynamodb"
+#     env    = "test"
+# }module "lambda" {
+#     source = "git::ssh://<YOUR_REPO>//modules/lambda"
+#     env    = "test"
+# }
